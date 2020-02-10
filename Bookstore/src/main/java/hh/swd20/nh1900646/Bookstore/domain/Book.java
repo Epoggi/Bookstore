bef925dcb35a963ff088b182fa3810d770f9f761
Epoggi <11327084+Epoggi@users.noreply.github.com>
2020-02-10 14:40:44 +0200

    Muutoksia mm. book.java täydennys ja thymeleaf ehkä

--- Bookstore/src/main/java/hh/swd20/nh1900646/Bookstore/domain/Book.java
+++ Bookstore/src/main/java/hh/swd20/nh1900646/Bookstore/domain/Book.java
@@ -1,5 +1,16 @@
 package hh.swd20.nh1900646.Bookstore.domain;
 
 public class Book {
-
+//Atribuutit
+	private String title;
+	private String author;
+	private int year;
+	private int isbn;
+	private double price;
+//Konstruktorit
+	
+//Getterit ja Setterit
+	
+//toString
+	
 }
--- Bookstore/src/main/java/hh/swd20/nh1900646/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/hh/swd20/nh1900646/Bookstore/web/BookController.java
@@ -1,5 +1,14 @@
 package hh.swd20.nh1900646.Bookstore.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
 public class BookController {
 
+	@RequestMapping(value = "/index")
+	public String frontPage() {
+		return "bookstorefrontpage";
+	}
 }
