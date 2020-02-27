7db30fc6c8e1f529e641440b2c71c4322c99a60f
Epoggi <11327084+Epoggi@users.noreply.github.com>
2020-02-27 15:03:16 +0200

    c3.4 tehtävä tehty

--- Bookstore/src/main/java/hh/swd20/nh1900646/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/hh/swd20/nh1900646/Bookstore/web/BookController.java
@@ -5,6 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -22,8 +24,11 @@ public class BookController {
 	public String frontPage() {
 		return "bookstorefrontpage";
 	}
+
 @Autowired
 BookRepository bookRepository;
+
+//Kirjojen listaus
 	@RequestMapping(value = "/books", method = RequestMethod.GET)
 	public String getAllBooks(Model model) {
 		// haetaan tietokannasta kirjat listaan
@@ -33,6 +38,30 @@ BookRepository bookRepository;
 		model.addAttribute("books", books);
 		return "booklist";
 
+	}
+	// tyhjän lomakkeen muodostaminen
+	@RequestMapping(value= "/newbook", method = RequestMethod.GET)
+	public String getNewBookForm (Model model) {
+		model.addAttribute("book",new Book());
+		return "bookform";
+	}
+	//syötettyjen tietojen vastaanotto
+	@RequestMapping(value= "/newbook", method = RequestMethod.POST)
+	public String saveBook(@ModelAttribute Book book) {
+	bookRepository.save(book);
+	return "redirect:/books";
+	}
+	// poisto
+	@RequestMapping(value= "/deletebook/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId) {
+		bookRepository.deleteById(bookId);
+		return "redirect:../books";
+	}
+	// Edit!
+	@RequestMapping(value="/newbook/{id}", method = RequestMethod.GET)
+	public String getBooktoEdit(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("book", bookRepository.findById(bookId));
+		return "bookform";
 	}
 
 }
