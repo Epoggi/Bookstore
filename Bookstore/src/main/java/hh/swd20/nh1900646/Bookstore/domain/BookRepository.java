82a46996ca2cd27b6933fb28462e63601fa74da2
Epoggi <11327084+Epoggi@users.noreply.github.com>
2020-03-10 11:12:27 +0200

    tehtävät 5.1 tehty

--- Bookstore/src/main/java/hh/swd20/nh1900646/Bookstore/domain/BookRepository.java
+++ Bookstore/src/main/java/hh/swd20/nh1900646/Bookstore/domain/BookRepository.java
@@ -2,12 +2,14 @@ package hh.swd20.nh1900646.Bookstore.domain;
 
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
 //CrudRepository-rajapinnan parametrisoinnissa annetaan Entity-luokan nimi: Book
 	// ja toisena parametrina pääavainsarakkeen luokkatietotyyppi, Long
 List <Book> findByTitle(String title);
 List <Book> findByAuthor(String author);
+Optional<Book> findById(Long id);
 	// BookRepository periytyy (extends) CrudRepositorystä ja perii mm.
 	// metodiesittelyt
 	// findAll(), findById(), save(), deleteById()
--- Bookstore/src/main/java/hh/swd20/nh1900646/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/hh/swd20/nh1900646/Bookstore/web/BookController.java
@@ -1,6 +1,7 @@
 package hh.swd20.nh1900646.Bookstore.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import hh.swd20.nh1900646.Bookstore.domain.Book;
 import hh.swd20.nh1900646.Bookstore.domain.BookRepository;
@@ -80,5 +83,15 @@ CategoryRepository categoryRepository;
 		model.addAttribute("categories", categoryRepository.findAll());
 		return "bookform";
 	}
-
+	//Restit
+	//Resti lista
+	@RequestMapping(value="restbooks",method=RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest(){
+		return (List<Book>) bookRepository.findAll();
+	}
+	//Return one with id
+	@RequestMapping(value="/book/{id}", method= RequestMethod.GET)
+	 public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long id) {	
+    	return bookRepository.findById(id);
+    }  
 }
